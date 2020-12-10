7ecc8ecb9946834e8f837269fcda35032b543132
yci infotech <you@example.com>
2020-12-10 14:08:15 +0530

    adding heroku new app

--- index.js
+++ index.js
@@ -7,14 +7,89 @@ const port=process.env.PORT || 3000;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+var mysql = require('mysql');
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'collegedatabase'
+});
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM admin_login WHERE email = ? AND pass = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				 ssn = req.session;
+				 req.session.loggedin = true;
+				 req.session.username = username;
+		         req.session.password = password;
+       
+        res.redirect('/aa');
+
+		//	res.redirect('/home');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+		
+	}
+});
+
+app.get('/home', function(req, res) {
+	if (req.session.loggedin) {
+		res.send('Welcome back, ' + req.session.username + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
+
+
+
+
+
+app.get('/aa', (req, res) => {
+	if (req.session.loggedin) {
   res.redirect(`/${uuidV4()}`)
+	}
 })
 app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
-  })
+	if (req.session.loggedin) {
+		var a=req.session.username;
+	res.render('room', { roomId: req.params.room,a:a })
+	}
+	else{
+		res.render('user', { roomId: req.params.room })	
+	}
+})
 
   io.on('connection', socket => {
+	
     socket.on('join-room', (roomId, userId) => {
       socket.join(roomId)
       socket.to(roomId).broadcast.emit('user-connected', userId)
@@ -22,7 +97,8 @@ app.get('/:room', (req, res) => {
       socket.on('disconnect', () => {
         socket.to(roomId).broadcast.emit('user-disconnected', userId)
       })
-    })
+	})
+
   })
 
 server.listen(port,()=>{console.log("server is listening on port ${port}")
--- public/script.js
+++ public/script.js
@@ -4,14 +4,17 @@ const videoGrid = document.getElementById('video-grid')
 //   host: '/',
 //   port: '3001'
 // })
+
 const myPeer=new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443})
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true
+  audio: false
 }).then(stream => {
+  userVideoStream=stream;
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
@@ -22,11 +25,16 @@ navigator.mediaDevices.getUserMedia({
     })
   })
 
+  
+  
   socket.on('user-connected', userId => {
     console.log("User Connected " + userId)
     connectToNewUser(userId, stream)
   })
+
 })
+
+
 
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
@@ -35,6 +43,10 @@ socket.on('user-disconnected', userId => {
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
 })
+
+
+
+
 
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
@@ -49,10 +61,63 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call
 }
 
+
+
+
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
   videoGrid.append(video)
+}
+
+const playa =()=>
+{
+  if(admin_id)
+  {
+    playStop();
+  }
+  else
+  {
+    playStop1();
+  }
+
+}
+  const playStop = ()=> {
+    let enabled=userVideoStream.getVideoTracks()[0].enabled;
+    if(enabled)
+    {
+      setStopVideo()
+      userVideoStream.getVideoTracks()[0].enabled=true; 
+    }
+    
+    }
+
+    const playStop1 = ()=> {
+      let enabled=userVideoStream.getVideoTracks()[0].enabled;
+      if(enabled)
+      {
+        setPlayVideo()
+        userVideoStream.getVideoTracks()[0].enabled=false; 
+      }
+      
+      }
+
+ 
+
+const setStopVideo =() =>{
+  const html = `
+    <i class="fas fa-video" ></i>
+    <span> Stop Video </span>
+  `
+  document.querySelector('.main__video_button').innerHTML=html;
+}
+
+const setPlayVideo =() =>{
+  const html = `
+    <i class="stop fas fa-video-slash" ></i>
+    <span> Play Video </span>
+  `
+  document.querySelector('.main__video_button').innerHTML=html;
 }
